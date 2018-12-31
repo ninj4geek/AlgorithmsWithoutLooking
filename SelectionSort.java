484ab52d4b7a5e47cc8c78f5c25f4ac2ef2420db
Patrick McClellan <pmcclellan1289@gmail.com>
2018-12-31 10:54:04 -0600

    Update SelectionSort.java

--- SelectionSort.java
+++ SelectionSort.java
@@ -7,41 +7,43 @@ public class SelectionSort {
         //fill it with random numbers.
 
         List<Integer> list = new ArrayList<Integer>();
-        for (int a = 0; a < 5; a++){
+        for (int a = 1; a <= 10; a++){
             double y = Math.random();  //get our decimal randoms
-            int x = (int)(y * 100);    //make them usable
+            int x = (int)(y * 100);    //make them usable ints
             list.add(x);               //add to list at position 'i'
-        }   System.out.println("Initial list:"+list);
+        }   System.out.println("\n\nInitial list: "+list+"\n\n");
 
         //Scan all items and find the smallest.
         //Swap it into position as the first item. (inner loop)
         //Repeat the selection sort on the remaining N-1 items. (outer loop)
 
         int indexj = 0; //have to put this here, had it inside the loop
-                        //which caused b to get assigned to pos 0
+                        //which caused b to get assigned to 0
                         //when b > a
 
         for (int i = 0; i < list.size()-1; i++) {
-            //initialize a with furthest left position
             int a = list.get(i);
-//            System.out.println("1a is now "+a+" at index "+i);
 
-
-            for (int j = i+1; j < list.size(); j++){
-//                System.out.println("Inner loop on index "+j);
+            for (int j = i+1; j < list.size(); j++) {
                 int b = list.get(j);
-                if (b < a){
+                
+                //find and store value, location of next smallest value
+                if (b <= a) {
                     a = b;
                     indexj = j;
-//                    System.out.println("2a is now "+a+" at index "+indexj);
                 }
             }
-            int temp = list.get(i);
-            list.set(i,a);
-            list.set(indexj,temp);
+            //if the next smallest value is actually smaller than the
+            //current position, then swap and print the test line
+            //had a bug here before, had to add this if test
+            if (list.get(i) > a) {
+                int temp = list.get(i);
+                list.set(i, a);
+                list.set(indexj, temp);
 
-            System.out.println("In process:  "+list);
+                System.out.println("In process i" + i + "  " + list);
+            }
         }
-        System.out.println("Sorted list: "+list);
+        System.out.println("Sorted list:   "+list);
     }
 }
